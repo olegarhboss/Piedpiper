99ecaa2058395ec9594f6513bb33c96f9580250d
Поляков Олег <webmasterolegan@gmail.com>
2020-03-08 07:42:14 +1000

    Изминение разметки служебных страниц

--- resources/js/views/help.vue
+++ resources/js/views/help.vue
@@ -1,1 +1,15 @@
-<template></template>
+<template>
+    <section>
+        <h1 class="text-center display-4 my-8">Справочная информация</h1>
+
+        <v-alert prominent type="info">
+            <v-row align="center">
+                <v-col class="grow">Документация по работе с API расположена на Swagger, она может содержать недостаточно информации об ответах сервера.</v-col>
+                    <v-col class="shrink">
+                        <v-btn color="primary" href="https://app.swaggerhub.com/apis-docs/webmasterolegan/TestTask/0.0.1" target="_blank">API Документация</v-btn>
+                    </v-col>
+            </v-row>
+        </v-alert>
+
+    </section>
+</template>
--- resources/js/views/index.vue
+++ resources/js/views/index.vue
@@ -2,25 +2,37 @@
     <div>
         <h1 class="text-center display-4 my-8">The Internet We Deserve</h1>
 
-        
+        <v-divider class="my-6"></v-divider>
 
+        <div class="text-center">
+            <v-btn x-large block color="warning" :to="{ name: 'posts' }" dark>Новости</v-btn>
+            <v-btn x-large block color="primary" class="mt-4" :to="{ name: 'help' }" dark>Помощь</v-btn>
+
+            <v-divider class="my-6"></v-divider>
         
+            <v-btn x-large block color="success" v-if="authUser" :to="{ name: 'profile' }" exact dark>Профиль пользователя</v-btn>
+            <v-btn x-large block color="success" v-else :to="{ name: 'login' }"  dark>Войти в систему</v-btn>
+        </div>
     </div>
 </template>
 
 <script>
+    import { mapGetters } from 'vuex';
+    
     export default {
         data: () => ({
-            page: 1,
-            posts: '',
+
         }),
 
-        methods: {
-
+        computed: {
+            ...mapGetters('auth', ['authUser'])
         },
 
-        mounted() {
-            
+        methods: {
+            logout() {
+                this.$store.dispatch('auth/logout');
+                this.$router.push({ name: 'main'});
+            }
         }
     }
 </script>
